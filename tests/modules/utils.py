318f5bacf1da65611d9cb89b98fa4ea5846c2b0e
Pavlin Poličar <pavlin.g.p@gmail.com>
2017-09-18 10:28:17 +0200

    Improve gradient check code

--- tests/modules/utils.py
+++ tests/modules/utils.py
@@ -1,18 +1,18 @@
 import numpy as np
 
 
-def numeric_gradient(f, x, eps=1e-6):
+def numeric_gradient(f, x, eps=1e-7):
     x, grad = x.astype(np.float64), np.zeros_like(x, dtype=np.float64)
 
     for idx in np.ndindex(x.shape):
-        initial_x = x[idx]
+        initial_value = x[idx]
 
-        x[idx] = initial_x + eps
+        x[idx] = initial_value + eps
         nudge_up = f(x)
-        x[idx] = initial_x - eps
+        x[idx] = initial_value - eps
         nudge_down = f(x)
         # Restore x to its previous state
-        x[idx] = initial_x
+        x[idx] = initial_value
 
         centered_diff = (nudge_up - nudge_down) / (2 * eps)
         grad[idx] = np.sum(centered_diff)
