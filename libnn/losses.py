e1ef4272530c6818a5537adfed9decd6f4215878
Pavlin Poličar <pavlin.g.p@gmail.com>
2017-09-20 10:41:43 +0200

    Add mse and bce losses

---
+++ libnn/losses.py
@@ -0,0 +1,44 @@
+from abc import ABCMeta, abstractmethod
+
+import numpy as np
+
+
+class Loss(metaclass=ABCMeta):
+    def __call__(self, *args, **kwargs):
+        return self.cost(*args, **kwargs)
+
+    @abstractmethod
+    def cost(self, y_hat, y):
+        pass
+
+    @abstractmethod
+    def gradient(self):
+        pass
+
+
+class MeanSquaredError(Loss):
+    def __init__(self):
+        self.__errors = None
+
+    def cost(self, y_hat, y):
+        errors = y_hat - y
+        self.__errors = errors
+        return errors.dot(errors) / errors.shape[0]
+
+    def gradient(self):
+        return self.__errors / self.__errors.shape[0]
+
+
+class BinaryCrossEntropy(Loss):
+    def __init__(self):
+        self.__y_hat = self.__y = None
+
+    def cost(self, y_hat, y):
+        self.__y_hat, self.__y = y_hat, y
+        cross_entropy = (y * np.log(y_hat)) + ((1 - y) * np.log(1 - y_hat))
+        return -np.mean(cross_entropy)
+
+    def gradient(self):
+        y_hat, y = self.__y_hat, self.__y
+        gradient = y / y_hat - (1 - y) / (1 - y_hat)
+        return -gradient / y_hat.shape[0]
---
+++ tests/test_losses.py
@@ -0,0 +1,45 @@
+import unittest
+from functools import partial
+
+import numpy as np
+
+from libnn.losses import MeanSquaredError, BinaryCrossEntropy
+from tests.modules.utils import numeric_gradient
+
+
+class TestMeanSquaredError(unittest.TestCase):
+    def test_cost(self):
+        y_hat = np.array([4, 6, 5.5, 5])
+        y = np.array([5, 5, 5, 5])
+
+        mse = MeanSquaredError()
+        self.assertEqual(mse(y_hat, y), 2.25 / 4)
+
+    def test_gradient(self):
+        y_hat = np.array([4, 6, 5.5, 5])
+        y = np.array([5, 5, 5, 5])
+
+        mse = MeanSquaredError()
+        mse(y_hat, y)
+        self.assertEqual(mse.gradient().shape, (4,))
+
+
+class TestBinaryCrossEntropy(unittest.TestCase):
+    def test_cost(self):
+        y_hat = np.array([.8, .75, .25, .01, .99])
+        y = np.array([1, 0, 0, 1, 1])
+
+        bce = BinaryCrossEntropy()
+        self.assertAlmostEqual(bce(y_hat, y), 1.3024, delta=1e-4)
+
+    def test_gradient(self):
+        y_hat = np.array([.8, .75, .25, .01, .99])
+        y = np.array([1, 0, 0, 1, 1])
+
+        bce = BinaryCrossEntropy()
+        bce(y_hat, y)
+
+        np.testing.assert_almost_equal(
+            bce.gradient(),
+            numeric_gradient(partial(bce, y=y), y_hat)
+        )
