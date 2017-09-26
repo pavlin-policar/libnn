ef519cdf950d4ee30985ceac22e6d0ad62b2e7c2
Pavlin Poličar <pavlin.g.p@gmail.com>
2017-09-26 10:41:29 +0200

    Fix bias initialization scale in linear layer

--- libnn/modules/layers.py
+++ libnn/modules/layers.py
@@ -9,8 +9,9 @@ class Linear(Module):
         self.in_features = in_features
         self.out_features = out_features
 
-        self.W = self.trainable(0.01 * np.random.randn(in_features, out_features))
-        self.b = self.trainable(np.random.randn(out_features))
+        scale = 0.01
+        self.W = self.trainable(scale * np.random.randn(in_features, out_features))
+        self.b = self.trainable(scale * np.random.randn(out_features))
 
     def forward(self, X):
         self.save_for_backward(X)
