97e27acd8a28833a3aae188486dd0ed11d9e73a5
Pavlin Poličar <pavlin.g.p@gmail.com>
2017-10-06 18:25:50 +0200

    Add zero and ones initialization

--- libnn/modules/initializations.py
+++ libnn/modules/initializations.py
@@ -11,3 +11,11 @@ def xavier(in_shape, out_shape):
 
 def xavier_relu(in_shape, out_shape):
     return np.random.randn(in_shape, out_shape) / np.sqrt(in_shape / 2)
+
+
+def ones(in_shape, out_shape):
+    return np.ones((in_shape, out_shape))
+
+
+def zeros(in_shape, out_shape):
+    return np.zeros((in_shape, out_shape))
