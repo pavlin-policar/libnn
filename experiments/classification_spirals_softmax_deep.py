0c8e9b8626308fa29a366a5e2d198425b3a65532
Pavlin Poličar <pavlin.g.p@gmail.com>
2017-10-06 19:05:53 +0200

    Train 10 layer ff net with batch norm for spirals

--- experiments/classification_spirals_softmax_deep.py
+++ experiments/classification_spirals_softmax_deep.py
@@ -4,50 +4,61 @@ from libnn.losses import CategoricalCrossEntropy
 from libnn.modules.activations import ReLU, Softmax
 from libnn.modules.layers import Linear
 from libnn.modules.module import Sequential
+from libnn.modules.normalization import BatchNormalization
 
 X, y = datasets.spirals(300)
 
 
 hidden_layer_size = 10
-initialization = 'xavier_relu'
+initialization = 'random_normal'
 
 
 loss = CategoricalCrossEntropy()
 
 model = Sequential(
     Linear(2, hidden_layer_size, init=initialization),
+    BatchNormalization(hidden_layer_size),
     ReLU(),
     Linear(hidden_layer_size, hidden_layer_size, init=initialization),
+    BatchNormalization(hidden_layer_size),
     ReLU(),
     Linear(hidden_layer_size, hidden_layer_size, init=initialization),
+    BatchNormalization(hidden_layer_size),
     ReLU(),
     Linear(hidden_layer_size, hidden_layer_size, init=initialization),
+    BatchNormalization(hidden_layer_size),
     ReLU(),
     Linear(hidden_layer_size, hidden_layer_size, init=initialization),
+    BatchNormalization(hidden_layer_size),
     ReLU(),
     Linear(hidden_layer_size, hidden_layer_size, init=initialization),
+    BatchNormalization(hidden_layer_size),
     ReLU(),
     Linear(hidden_layer_size, hidden_layer_size, init=initialization),
+    BatchNormalization(hidden_layer_size),
     ReLU(),
     Linear(hidden_layer_size, hidden_layer_size, init=initialization),
+    BatchNormalization(hidden_layer_size),
     ReLU(),
     Linear(hidden_layer_size, hidden_layer_size, init=initialization),
+    BatchNormalization(hidden_layer_size),
     ReLU(),
     Linear(hidden_layer_size, hidden_layer_size, init=initialization),
+    BatchNormalization(hidden_layer_size),
     ReLU(),
     Linear(hidden_layer_size, 3, init=initialization),
     Softmax(),
 )
 
 
-learning_rate = 0.03
-for epoch in range(5000):
+learning_rate = 1
+for epoch in range(500):
     # Perform the forward pass
     z = model(X)
 
     # Compute the error
     error = loss(z, y)
-    print('Epoch %2d: %.4f' % (epoch, error))
+    print('Epoch %4d: %.4f' % (epoch, error))
 
     # Backpropagate the gradient
     gradient = loss.gradient()
